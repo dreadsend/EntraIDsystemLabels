98a733ed84959ecdeba76e3517f8f709d0f701a5
dreadsend <juliansperling@gmail.com>
2023-12-09 18:15:49 +0100

    First Release

--- README.md
+++ README.md
@@ -1,2 +1,73 @@
-# EntraIDsystemLabels
-A Repository to Document possible Values and their context around the Entra ID Device Property "systemLabels"
+This Repository was created to document the possible values of the [Microsoft Graph Device Property](https://learn.microsoft.com/en-us/graph/api/resources/device?view=graph-rest-1.0#properties) "systemLabels", since the official Documentation is not quite clear.
+
+What we do know is that the Property is read-only and used by Microsoft internally.
+> [From Entra ID dynamic Group Rules for devices](https://learn.microsoft.com/en-us/entra/identity/users/groups-dynamic-membership#rules-for-devices)   
+> Note  
+> systemlabels is a read-only attribute that cannot be set with Intune.
+
+In hope that Cunninghams Law is true, I thought it might be a good idea for someone to try and keep a public collection.
+ 
+<br class="">
+  
+
+# Known Values
+
+| Value | Use / Appearance | Submitter | Verified |
+|---|---|---|---|
+| AzureResource | &#124; [Windows VMs in Azure enabled with Microsoft Entra sign-in](https://learn.microsoft.com/en-us/entra/identity/devices/howto-vm-sign-in-azure-ad-windows) | &#124; Author | &#124; [Conditional Acces Documentation](https://learn.microsoft.com/en-us/entra/identity/conditional-access/concept-condition-filters-for-devices#supported-operators-and-device-properties-for-filters), not seen in the wild yet  |
+| M365Managed | &#124; [Devices managed using Microsoft Managed Desktop](https://learn.microsoft.com/en-us/managed-desktop/overview/service-plan) | &#124; Author | &#124; [Conditional Acces Documentation](https://learn.microsoft.com/en-us/entra/identity/conditional-access/concept-condition-filters-for-devices#supported-operators-and-device-properties-for-filters), not seen in the wild yet |
+| MultiUser | &#124; [Shared devices](https://learn.microsoft.com/en-us/entra/identity-platform/msal-shared-devices)  | &#124; Author | &#124; [Conditional Acces Documentation](https://learn.microsoft.com/en-us/entra/identity/conditional-access/concept-condition-filters-for-devices#supported-operators-and-device-properties-for-filters), not seen in the wild yet |
+| ...? | &#124; | &#124; | &#124; |
+
+ 
+<br class="">
+  
+
+
+# Add more
+
+If you would like to add more values or have a description corrected (or maybe even direct me to a complete official documentation), please raise a GitHub Issue (or a pull request) with a screenshot of the new Value and a guess as to what the usage might be.
+ 
+<br class="">
+  
+If you would like to find out what the values in your Environments are, I have provided an example Script that pulls from your Environment
+ 
+<br class="">
+  
+## informationCollector Prerequisites
+
+1. PowerShell Modules
+    - Microsoft.Graph.Authentication
+2. GraphAPI Permissions on the Graph PowerShell, or A Custom Enterprise App
+    - Device.Read.All
+3. If Using Delegate Permission
+    - User with Global Reader or a Role that can show Devices
+4. A privileged Auth / Global Admin to Consent to the API Permissions
+
+ 
+<br class="">
+  
+
+## informationCollector Usage
+
+The Scipt mirrors most Authentication Flows of Connect-MgGraph. By default it will ask for the credentials interactively.
+
+```powershell
+.\informationCollector -tenantId "..." -clientId "..."
+
+.\informationCollector -tenantId "..." -clientId "..." -certificateThumbprint "..."
+
+# Will Prompt you for the Secret of your App ID - consider using a certificate
+.\informationCollector -tenantId "..." -clientId "..." -useSecret
+
+# Will go though the Device Code flow, should be used by default in for Example Cloud Shell
+.\informationCollector -deviceCode
+```
+ 
+<br class="">
+  
+By Default you will get a short List of all systemLabels found in your Environment with a single Example Devicy displayName.   
+
+
+If you want the script to return a full list of all Devices with the associated labels, use  `.\informationCollector -fullList`     
+This should also be used in environments with a lot of Devices to skip the simplification, since reducing the List requires parsing all returned entries.
---
+++ informationCollector.ps1
@@ -0,0 +1,143 @@
+<#
+.SYNOPSIS
+  Gives a Report of all systemLabels Values in a Tenant
+.DESCRIPTION
+  The Script will connect to the Graph API in the manner specified and fetch the Information of all Devices that have a systemLabel set
+  A large block of Connect-MgGraph Parameters are Mirrored
+
+.PARAMETER useSecret
+  Switch to prompt for a Secret in Combination with tenantId and clientId instad of a certificate Thumbprint
+
+.PARAMETER fullList
+  Returns the full list of the Devices instead of simplifying
+
+.NOTES
+  Version:        1.0
+  Author:         Julian Sperling
+  Creation Date:  09.12.23
+  Purpose/Change: Initial script development
+  
+.EXAMPLE
+  <Example goes here. Repeat this attribute for more than one example>
+#>
+
+param(
+    [Parameter(ParameterSetName = "Interactive")]
+
+    [Parameter(ParameterSetName = "CustomApp", Mandatory = $true)]
+    [Parameter(ParameterSetName = "ClientCert", Mandatory = $true)]
+    [Parameter(ParameterSetName = "ClientCredentials", Mandatory = $true)]
+    [string]$tenantId,
+
+    [Parameter(ParameterSetName = "CustomApp", Mandatory = $true)]
+    [Parameter(ParameterSetName = "ClientCert", Mandatory = $true)]
+    [Parameter(ParameterSetName = "ClientCredentials", Mandatory = $true)]
+    [string]$clientId,
+
+    [Parameter(ParameterSetName = "ClientCert", Mandatory = $true)]
+    [ValidateNotNull()]
+    [string]$certificateThumbprint,
+
+    [Parameter(ParameterSetName = "ClientCredentials", Mandatory = $true)]
+    [switch]$useSecret,
+
+    [Parameter(ParameterSetName = "DeviceCode", Mandatory = $true)]
+    [switch]$deviceCode,
+
+    [switch]$fullList
+
+)
+
+$requiredModules = @('Microsoft.Graph.Authentication')
+$requiredScopes = @('Device.Read.All')
+
+$deviceProperties = @('displayName', 'systemLabels')
+
+# ArrayList is a reference type, so the base object is always modified, no return necessary
+function Get-GraphData {
+    param (
+        [Parameter(Mandatory = $true)]
+        [string]$Uri,
+        [hashtable]$headers = @{},
+
+        [System.Collections.Arraylist]$result
+    )
+
+    # Invoke Graph API request
+    $response = Invoke-MgGraphRequest -Method GET -Uri $Uri -Headers $headers -OutputType PSObject
+
+    $result.AddRange($response.value)
+
+    # Check for the next link and recursively fetch data
+    if ($response.'@odata.nextLink') {
+        Get-GraphData -Uri $response.'@odata.nextLink' -headers $headers -result $result
+    }
+}
+
+
+
+# Check for Required Modules
+$installedModules = Get-Module -ListAvailable | Select-Object -ExpandProperty Name
+$missingModules = $requiredModules | Where-Object { $_ -notin $installedModules }
+
+if ($missingModules) { 
+    Write-Host "The following modules are missing: $($missingModules -join ', ') `nPlease Install the Module(s) and try again"
+    return
+}
+
+
+# Connect to Graph API depending on Script Parameters Used
+switch ($PSCmdlet.ParameterSetName) {
+    "Interactive" {
+        Connect-MgGraph -Scopes $requiredScopes -NoWelcome
+    }
+    "ClientCert" {
+        Connect-MgGraph -TenantId $tenantId -ClientId $clientId -CertificateThumbprint $certificateThumbprint -NoWelcome
+    }
+    "ClientCredentials" {
+        $ClientSecretCredential = Get-Credential -Credential $clientId
+        Connect-MgGraph -TenantId $tenantId -ClientSecretCredential $ClientSecretCredential -NoWelcome
+    }
+    "DeviceCode" {
+        Connect-MgGraph -Scopes $requiredScopes -UseDeviceAuthentication -NoWelcome
+    }
+    "CustomApp" {
+        Connect-MgGraph -TenantId $tenantId -ClientId $clientId -NoWelcome
+    }
+}
+
+
+# Check if all Required Scopes are Present
+$missingScopes = $requiredScopes | Where-Object { $_ -notin $(Get-MgContext).Scopes }
+if ($missingScopes) {
+    Write-Host "The following Scopes are missing: $($missingScopes -join ', ')"
+    Write-Host "Please elevate to a Device / Global Reader Entra ID Role or add the Scope(s) to your Enterprise App and consent."
+    return
+}
+
+$devices = [System.Collections.Arraylist]::new()
+
+# It feels wrong to me to fetch all Devices and start processing them locally if the API Supports Filters that give us what we want
+# We have to tell the Graph API that we are using count ($count=true) and accept a lower consistency Level
+$uri = 'https://graph.microsoft.com/v1.0/devices?$filter=systemLabels/$count ne 0&$count=true&$select={0}' -f $($deviceProperties -join ',')
+
+Get-GraphData -Uri $uri -result $devices -headers @{ ConsistencyLevel = "eventual" }
+
+# If the Full list was requested we skip the simplification
+if ($fullList) {return $devices}
+
+$result = @{}
+
+# For simplicity and speed I only store one example for each value
+foreach ($dev in $devices){
+    foreach ($entry in $dev.systemLabels){
+        $result.$entry = $($dev.displayName)
+    } 
+}
+
+Write-Host ("These are the System Labels in your environment with the Displayname of an example device")
+Write-Host ("If you want the full list use the -fullList Parameter of the script")
+Write-Host ("Please check if there is an unkdocumented value. Let me know what the value is and what you presume it might be used for ;)")
+$result.Keys | Select-Object @{Name = "systemLabel"; Expression = {$_} }, @{Name = "Example"; Expression = {$result.$_} } | Format-Table -AutoSize
+
+Read-Host
